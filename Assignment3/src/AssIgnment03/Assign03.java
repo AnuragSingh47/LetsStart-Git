0d573c72dbabe1bc79a5e46984822958833f84c5
Anurag Singh <Anurag Singh@AnuragSingh>
2023-11-24 09:54:52 -0500

    Resolved Code

--- Assignment3/src/AssIgnment03/Assign03.java
+++ Assignment3/src/AssIgnment03/Assign03.java
@@ -2,16 +2,7 @@ package AssIgnment03;
 
 public class Assign03 {
 	public static void main(String[] args) {
-		// program rolls a 20 sided dice 1000 times and records the number
-		// of occurrences of each result.
-		// See:https://en.wikipedia.org/wiki/Dice#Common_variations
-		// Math.random() returns 0.0 to 1.0 excluding 1.0
-		// i.e. we could get 0.99999999999 etc. but not 1.0
-		// so Math.random() times upper range of 20 is 0 to 19
-
-		// Hint: it is easy to make a mistake with array indexes as people
-		// (and dice) count from 1 upwards, while programmers using arrays
-		// count from 0 upwards.
+		
 		int roll = 0;
 		int[] diceRolls = new int[20];
 		int totalRolls = 0;
