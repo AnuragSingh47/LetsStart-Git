ff6dc2ec08c3814ab92cdd81a5d9925aa6dee156
Anurag Singh <Anurag Singh@AnuragSingh>
2023-11-24 10:07:39 -0500

    Code with error

--- Assignment3/src/AssIgnment03/Assign03.java
+++ Assignment3/src/AssIgnment03/Assign03.java
@@ -2,13 +2,22 @@ package AssIgnment03;
 
 public class Assign03 {
 	public static void main(String[] args) {
-		
+		// program rolls a 20 sided dice 1000 times and records the number
+		// of occurrences of each result.
+		// See:https://en.wikipedia.org/wiki/Dice#Common_variations
+		// Math.random() returns 0.0 to 1.0 excluding 1.0
+		// i.e. we could get 0.99999999999 etc. but not 1.0
+		// so Math.random() times upper range of 20 is 0 to 19
+
+		// Hint: it is easy to make a mistake with array indexes as people
+		// (and dice) count from 1 upwards, while programmers using arrays
+		// count from 0 upwards.
 		int roll = 0;
 		int[] diceRolls = new int[20];
 		int totalRolls = 0;
 
 		// sample the rolls
-		for(int count = 0; count < 10; count++) {
+		for(int count = 0; count < 1000; count++) {
 			roll = (int)(Math.random() * 20) + 1;
 			diceRolls[roll] = diceRolls[roll - 1] + 1; // crashes
 		}
@@ -18,7 +27,6 @@ public class Assign03 {
 			totalRolls = totalRolls + diceRolls[index];
 		}
 		System.out.println("Total rolls were: " + totalRolls);
-		System.out.println("I am Anurag Singh" );
 	}
 
 }
